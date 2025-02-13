510e4b856ba2d97552ba363e10fa2fa24f66ad99
ruskinl <165720407+ruskinl@users.noreply.github.com>
2025-02-12 23:49:01 -0500

    Update matViewCapacity.sql

--- SQL Files/matViewCapacity.sql
+++ SQL Files/matViewCapacity.sql
@@ -6,16 +6,20 @@ SELECT
     c.Year,
     c.Term,
     cr.Capacity,
+	COUNT_BIG(*) AS TotalRecords,
     COUNT_BIG(e.StudentID) AS EnrolledCount,
     COUNT_BIG(tc.CourseID) AS PrereqCoursesCompleted
-FROM Courses c
-JOIN Classroom cr ON c.ClassroomID = cr.ClassroomID  -- capacity from Classroom table
-LEFT JOIN Enroll e ON c.CourseID = e.CourseID  -- countig enrolled students
-LEFT JOIN TakenCourses tc ON e.StudentID = tc.StudentID  -- counting prerequisites completed
+FROM dbo.Courses c
+JOIN dbo.Classroom cr ON c.ClassroomID = cr.ClassroomID  -- capacity from Classroom table
+JOIN dbo.Enroll e ON c.CourseID = e.CourseID  -- countig enrolled students
+JOIN dbo.TakenCourses tc ON e.StudentID = tc.StudentID  -- counting prerequisites completed
 GROUP BY c.CourseID, c.Year, c.Term, cr.Capacity;
 GO
 
 -- unique clustered index
 CREATE UNIQUE CLUSTERED INDEX IDX_vwEnrollCapacity_CourseID
 ON vwEnrollCapacity (CourseID, Year, Term);
+GO
+
+DROP VIEW IF EXISTS vwEnrollCapacity;
 GO
