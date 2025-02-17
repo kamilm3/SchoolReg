70b8859a93e06c23775f02196fc226968a6edffb
ruskinl <165720407+ruskinl@users.noreply.github.com>
2025-02-17 00:38:27 -0500

    New Mat View that joins prereq and taken courses

---
+++ SQL Files/vwPrereqCheck.sql
@@ -0,0 +1,14 @@
+create view vwPrereqCheck
+with SCHEMABINDING
+as
+select p.CourseID,
+    tc.StudentID,
+    count_big(*) as PrereqsCompleted
+from dbo.Prereq p
+join dbo.TakenCourses tc on p.PrereqID = tc.CourseID
+GROUP BY p.CourseID, tc.StudentID;
+go
+
+CREATE UNIQUE CLUSTERED INDEX IDX_vwPrereqEligibility_cidSID
+ON vwPrereqCheck (CourseID, StudentID);
+GO
